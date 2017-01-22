df7a9d2dc8b3429f61036e8b44f7312c5fd6c997
k777i <k777ig@gmail.com>
2017-01-22 19:12:04 +0200

    Создаем Router

--- www/functions/sql.php
+++ www/functions/sql.php
@@ -1,1 +1,16 @@
 <?php
+
+function sqlConnect(){
+    $connect = mysqli_connect("localhost", "root", "", "test_db");
+    return $connect;
+}
+
+function sqlQuery($sql){
+    $connect = sqlConnect();
+    $query = mysqli_query($connect, $sql);
+    $result = [];
+    while($row = mysqli_fetch_assoc($query)){
+        $result[]=$row;
+    }
+    return $result;
+}
--- www/models/photo.php
+++ www/models/photo.php
@@ -1,16 +1,7 @@
 <?php
-
+require __DIR__.'/../functions/sql.php';
 function Photo_getAll()
 {
-    $connect = mysqli_connect("localhost", "root", "", "test_db");
     $sql = "SELECT * FROM images";
-    $query = mysqli_query($connect, $sql);
-    $result = [];
-
-    while($row = mysqli_fetch_assoc($query)){
-        $result[]=$row;
-    }
-
-    return $result;
-
+    return sqlQuery($sql);
 }
